f9c7976aa2f5c80767fa007e1b90295673b68867
Programmation <Programmation@Mathieu-PC.home>
2018-04-06 16:47:10 +0200

    Changer name for PSR

---
+++ model/Acceuil.php
@@ -0,0 +1,15 @@
+<?php
+
+class Acceuil{
+    protected $infos;
+    
+    function getInfos() {
+        
+        $db = DBfactory::Getinstance();
+        $req = $db -> query ('SELECT name,surname,chapo,email,adress,github,linkedin,twitter FROM users WHERE userlvl="1" ');
+        $result=$req -> fetch(PDO::FETCH_ASSOC);
+        
+        return $result;
+    }
+    
+}
