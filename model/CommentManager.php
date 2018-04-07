965ecce4088dde8fb3ccafca08c6c3bcfa7702b0
Programmation <Programmation@Mathieu-PC.home>
2018-04-07 16:30:37 +0200

    Fixed bugs with validation and supression

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -12,15 +12,15 @@
  * @author Programmation
  */
 class CommentManager {
-    
+
     protected $Pman;
-    
+
     public function __construct() {
         $this->Pman = new PostsManager;
     }
-    
-    public function add_comment(array $comment){
-        if(empty($_POST['comment']) or empty($_POST['email']) or empty($_POST['first_name']) or empty($_POST['last_name']) or empty($_POST['postid'])){
+
+    public function add_comment(array $comment) {
+        if (empty($_POST['comment']) or empty($_POST['email']) or empty($_POST['first_name']) or empty($_POST['last_name']) or empty($_POST['postid'])) {
             return false;
         }
         $db = DBfactory::Getinstance();
@@ -32,40 +32,48 @@ class CommentManager {
             'first_name' => $_POST['first_name'],
             'last_name' => $_POST['last_name'],
             'postid' => $_POST['postid']
-            ));
+        ));
         return true;
     }
-    
-     public function getComments($postid){
+
+    public function getComments($postid) {
         $db = DBfactory::Getinstance();
         $req = $db->prepare('SELECT * FROM comments WHERE postid=:postid AND valid=1');
         $req->execute(array(
             'postid' => $postid
         ));
-        while($donnees=$req->fetch(PDO::FETCH_ASSOC)){
-            $result[] = $donnees; 
+        while ($donnees = $req->fetch(PDO::FETCH_ASSOC)) {
+            $result[] = $donnees;
         }
         return $result;
     }
-    
-      public function getUnvalid_Comments(){
+
+    public function getUnvalid_Comments() {
         $db = DBfactory::Getinstance();
         $req = $db->prepare('SELECT * FROM comments WHERE valid=0');
         $req->execute();
         $i = 0;
-        while($data=$req->fetch(PDO::FETCH_ASSOC)){
+        while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
             $post = $this->Pman->GetPost($data['postid']);
             $result[] = $data;
-            $result[$i][title]=$post['title'];
+            $result[$i][title] = $post['title'];
             $i++;
         }
         return $result;
-     }
-        
-      public function valid_comment($comment_id){
-         $db = DBfactory::Getinstance();
-         $req=$db->prepare('UPDATE comments SET valid=1 WHERE comment_id= ?');
-         $req->execute(array($comment_id));
-         return;
-     }
+    }
+
+    public function valid_comment(array $comments) {
+        $db = DBfactory::Getinstance();
+        foreach ($comments as $key => $value) {
+            if ($value == "Valider") {
+                $req = $db->prepare('UPDATE comments SET valid=1 WHERE comment_id= ?');
+                $req->execute(array($key));
+            } elseif ($value == "Supprimer") {
+                $req = $db->prepare('DELETE FROM comments WHERE comment_id= ?');
+                $req->execute(array($key));
+            }
+        }
+        return;
+    }
+
 }
